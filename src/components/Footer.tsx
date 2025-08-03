50e27317d4a6e8127ecf38bb893d458cbe6bf3c5
kishanshettyb <kishanqr@gmail.com>
2025-08-03 20:10:38 +0530

    fix: fix issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,11 +11,7 @@ export const Footer = () => {
           © {config.organization} {new Date().getFullYear()}
         </div>
         <div className="text-xs text-muted-foreground hidden lg:block">
-          <Link
-            href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
-          >
-            Blog powered by wisp
-          </Link>
+          <Link href="#">Blog powered by Dhruva Marketing</Link>
         </div>
         <Link href="/rss">
           <Button variant="ghost">
